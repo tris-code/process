4950d823928f32ff79c66a6aa84daa6c956293cf
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-04-05 20:41:26 +0000

    Update project urls

--- Package.swift
+++ Package.swift
@@ -8,22 +8,22 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/tris-foundation/platform.git",
+            url: "https://github.com/tris-code/platform.git",
             .branch("master")),
         .package(
-            url: "https://github.com/tris-foundation/time.git",
+            url: "https://github.com/tris-code/time.git",
             .branch("master")),
         .package(
-            url: "https://github.com/tris-foundation/async.git",
+            url: "https://github.com/tris-code/async.git",
             .branch("master")),
         .package(
-            url: "https://github.com/tris-foundation/fiber.git",
+            url: "https://github.com/tris-code/fiber.git",
             .branch("master")),
         .package(
-            url: "https://github.com/tris-foundation/aio.git",
+            url: "https://github.com/tris-code/aio.git",
             .branch("master")),
         .package(
-            url: "https://github.com/tris-foundation/test.git",
+            url: "https://github.com/tris-code/test.git",
             .branch("master"))
     ],
     targets: [
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Improved fiber-friendly Foundation.Process
 ## Package.swift
 
 ```swift
-.package(url: "https://github.com/tris-foundation/process.git", .branch("master"))
+.package(url: "https://github.com/tris-code/process.git", .branch("master"))
 ```
